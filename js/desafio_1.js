5936570125de9bf855353ea17dc07f3847cd4461
SergioMerino2021 <100006618.joan23@fje.edu>
2021-01-23 23:12:55 +0100

    RAIZ 23/01/2021

---
+++ js/desafio_1.js
@@ -0,0 +1,10 @@
+function val_1() {
+    var a = document.getElementById('val-1').value
+    
+    if (a == 'DHCP') {
+        return true
+    } else {
+        alert('FALLO DE LA RED')
+        return false
+    }
+}
