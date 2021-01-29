69fe27893afe5c1920dea5929baa5102273324a9
SergioMerino2021 <100006618.joan23@fje.edu>
2021-01-29 16:26:18 +0100

    Act.1 29.01.2021

--- docs/sonido/ttsscape.py
+++ docs/sonido/ttsscape.py
@@ -1,6 +1,7 @@
 import pyttsx3 as tts
 
-text=input("Text: ")
+#text=input("Text: ")
+text="Hola, estas en el terminal A12 con el sistema HEROS. Actualmente el sistema está comprometido debido al paso del tiempo. Para la reparación del sistema es necesario la restauración manual."
 path="voz1.mp3"
 engine = tts.init()
 engine.save_to_file(text, path )
